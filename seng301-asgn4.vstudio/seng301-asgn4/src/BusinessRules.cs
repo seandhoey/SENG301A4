d4de5381ab9a87f51c32687cc6b0efd4357dd42c
seandhoey <seanhoey@live.ca>
2017-04-05 15:54:36 -0600

    Fix

--- seng301-asgn4.vstudio/seng301-asgn4/src/BusinessRules.cs
+++ seng301-asgn4.vstudio/seng301-asgn4/src/BusinessRules.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using Frontend4;
+using Frontend4;
 using Frontend4.Hardware;
 using System;
 using System.Collections.Generic;
@@ -74,80 +73,3 @@ namespace seng301_asgn4.src
         }
     }
 }
-=======
-﻿using Frontend4;
-using Frontend4.Hardware;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace seng301_asgn4.src
-{
-    /**
-     * Class representing most of the internal software for a Vending Machine. Handles background tasks for facades
-     */
-    public class BusinessRules
-    {
-        private CommunicationFacade communicationFacade;
-        private PaymentFacade paymentFacade;
-        private ProductFacade productFacade;
-
-        public int creditInserted = 0;
-        private List<Coin> addedCoins = new List<Coin>();
-
-        /**
-         * Default constructor
-         */
-        public BusinessRules()
-        { //Reference made, but values not initialized
-        }
-
-        /**
-         * Initialize the facade references
-         */
-        public void init(CommunicationFacade communicationFacade, PaymentFacade paymentFacade, ProductFacade productFacade)
-        {
-            this.communicationFacade = communicationFacade;
-            this.paymentFacade = paymentFacade;
-            this.productFacade = productFacade;
-        }
-
-        /**
-         * Keep track of credit (not in hardware)
-         */
-        public void addCredit(int amount)
-        {
-            this.creditInserted += amount;
-            this.communicationFacade.addedCredit(creditInserted); //Update communication facade
-        }
-
-        /**
-         * Keep track of what coins have been inserted
-         */
-        public void addCoins(Coin coin)
-        {
-            this.addedCoins.Add(coin);
-            Cents cents = coin.Value; //Weird work around
-            this.creditInserted += (cents.Value);
-            this.communicationFacade.addedCredit(creditInserted); //Update communication facade
-        }
-
-        /**
-         * coinkRacks is an array containing each coin rack, ordered by the configured coinKinds
-         */
-        public void buttonPressed(int index, ProductKind productKind, int cost, CoinRack[] coinRacks)
-        {
-            //Currently, button exists and we have enough credit inserted
-            this.productFacade.dispenseProduct(index);
-            List<int> coinIndices = new List<int>();
-
-            //TODO:
-            //calculate change
-            //request payment facade to dispense change if applicable
-            this.paymentFacade.dispenseChange(coinIndices);
-        }
-    }
-}
->>>>>>> d4993314e84335080b718052a7ba32d4705132bb
--- seng301-asgn4.vstudio/seng301-asgn4/src/ProductFacade.cs
+++ seng301-asgn4.vstudio/seng301-asgn4/src/ProductFacade.cs
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-﻿using Frontend4.Hardware;
+
+using Frontend4.Hardware;
 using Frontend4;
 using System;
 using System.Collections.Generic;
@@ -56,62 +56,3 @@ namespace seng301_asgn4.src
         }
     }
 }
-=======
-﻿using Frontend4.Hardware;
-using Frontend4;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace seng301_asgn4.src
-{
-    public class ProductFacade
-    {
-        private HardwareFacade hardwareFacade;
-        private BusinessRules businessRules;
-
-        private Dictionary<SelectionButton, int> selectionButtonToIndex;
-
-        /**
-         * Constructor
-         */
-        public ProductFacade(HardwareFacade hardwareFacade, BusinessRules businessRules)
-        {
-            this.hardwareFacade = hardwareFacade;
-            this.businessRules = businessRules;
-
-            this.selectionButtonToIndex = new Dictionary<SelectionButton, int>();
-            for (int i = 0; i < this.hardwareFacade.SelectionButtons.Length; i++)
-            {
-                this.hardwareFacade.SelectionButtons[i].Pressed += new EventHandler(selectionPressed);
-                this.selectionButtonToIndex[this.hardwareFacade.SelectionButtons[i]] = i;
-            }
-        }
-
-        /**
-         * Let business rules know about button selection
-         */
-        public void selectionPressed(object sender, EventArgs e)
-        {
-            int index = this.selectionButtonToIndex[(SelectionButton)sender]; //Error here if pressed a button that doesn't exist
-            var productKind = this.hardwareFacade.ProductKinds[index];
-            var cents = productKind.Cost; //ProductKind is an object containing a string name and a Cents value
-            int cost = (cents.Value);
-            if (this.businessRules.creditInserted >= cost) //Do we have enough money?
-            {
-                this.businessRules.buttonPressed(index, productKind, cost, this.hardwareFacade.CoinRacks); //If so, let business rules handle
-            } //else ignore
-        }
-
-        /**
-         * Dispenses product requested by business rules
-         */
-        public void dispenseProduct(int index)
-        {
-            //TODO: implement
-        }
-    }
-}
->>>>>>> d4993314e84335080b718052a7ba32d4705132bb
